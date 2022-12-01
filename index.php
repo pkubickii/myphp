fa79e5156d57d7c5e34f4a94c2dcfbc2ec655258
pkubickii <kubicki.przemyslaw@gmail.com>
2022-12-01 01:00:49 +0100

    login form

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+session_start();
+$username = $_SESSION["username"];
+?>
 <!doctype html>
 <html>
 
@@ -8,39 +12,72 @@
   <script src="src/js/handlers.js"></script>
 </head>
 
-<body class="h-screen w-screen">
-  <header class="h-[10%] bg-zinc-400 flex flex-row items-center justify-end">
-    <div class="">
-      <div class="p-3 mx-4 w-auto rounded-xl shadow-2xl shadow-zinc-900 cursor-pointer h-2/4 hover:skew-y-3 hover:scale-110 duration-500 bg-yellow-300 text-2xl font-sans inline-block text-violet-700" onclick="handleClick()">Last load: <?php echo date("H:i:s"); ?></div>
-    </div>
+<body>
+  <div class="min-h-screen flex flex-col">
+    <nav class="flex flex-wrap items-center justify-between bg-gradient-to-tl from-zinc-200 to-zinc-900 p-3 sm:p-6">
+      <div class="p-3 mx-4 w-auto rounded-xl shadow-2xl shadow-zinc-900 cursor-pointer h-2/4 hover:skew-y-3 hover:scale-110 duration-500 bg-yellow-300 text-2xl font-sans inline-block text-violet-700" onclick="handleClick()">
+        Last load: <?= date("H:i:s"); ?>
+      </div>
+      <div className="flex flex-grow flex-row items-center justify-center md:flex-none">
+        <button class="h-10 w-28 rounded-md border-2 border-black bg-inherit from-sky-100 to-sky-500 px-2 py-1 text-xl shadow-lg hover:bg-gradient-to-br md:h-16 md:w-auto md:px-6 md:py-2" onclick="handleClick()">Register</button>
+      </div>
+    </nav>
+    <main class="flex-1 bg-gradient-to-br from-zinc-100 to-zinc-800">
+      <div class="relative overflow-hidden mb-8">
+        <div class="rounded-t-lg overflow-hidden border-t border-l border-r border-gray-400 flex justify-center p-8">
 
-  </header>
-  <main class="bg-gradient-to-br from-zinc-300 to-zinc-900 h-[80%]">
+          <form action="src/php/login.php" method="POST" class="w-full max-w-sm flex flex-col gap-3 border-2 p-10 border-gray-500">
+            <div class="flex flex-row">
+              <div class="w-1/3">
+                <label for="username" class="block text-gray-500 font-bold text-right mb-1 md:mb-0 pr-4">User name:</label>
+              </div>
+              <div class="w-2/3">
+                <input name="username" id="username" type="text" class="bg-gray-400 appearance-none border-2 border-gray-500 rounded-md w-full py-2 px-4 text-gray-800 leading-tight focus:outline-none focus:bg-white focus:border-amber-700">
+              </div>
+            </div>
 
-    <div id="login_form" class="flex flex-wrap items-center justify-center p-6">
-      <form action="src/php/login.php" method="POST">
-        <div class="flex flex-col w-24">
-          <label for="username">User name:</label>
-          <input name="username" type="text">
-          <label for="password">Password: </label>
-          <input name="password" type="password">
-          <button type="submit" class="border-2 font-sans my-3 py-2 rounded-md border-none shadow-zinc-600 hover:scale-105 hover:shadow-lime-500 shadow-xl hover:border-lime-400 bg-amber-700 text-zinc-300 hover:text-lime-400 text-2xl tracking-widest ">Login</button>
+            <div class="flex flex-row">
+              <div class="w-1/3">
+                <label for="password" class="block text-gray-500 font-bold text-right mb-1 md:mb-0 pr-4">Password: </label>
+              </div>
+              <div class="w-2/3">
+                <input name="password" id="password" type="password" placeholder="******************" class="bg-gray-400 appearance-none border-2 border-gray-500 rounded-md w-full py-2 px-4 text-gray-800 leading-tight focus:outline-none focus:bg-white focus:border-amber-700">
+              </div>
+            </div>
+            <div class="flex flex-row">
+              <div class="md:w-1/3"></div>
+              <div class="md:w-2/3">
+                <button type="submit" class="border-2 font-sans my-3 px-6 py-2 rounded-md border-none shadow-slate-600 hover:scale-105 hover:shadow-lime-500 shadow-xl hover:border-lime-400 bg-amber-700 text-zinc-300 hover:text-lime-400 text-2xl tracking-widest">Login</button>
+              </div>
+            </div>
+          </form>
+
         </div>
-      </form>
-    </div>
+      </div>
 
-  </main>
+      <?php if (isset($username)) { ?>
+        <div class="text-center text-4xl text-rose-500">You're logged in as: <?= $username ?></div>
+      <?php }
+      print_r($_SESSION);
+      echo "<br>";
+      echo "session uname: " . $_SESSION["username"];
+      echo "<br>";
+      echo "session pname: " . $_SESSION["password"];
+      echo "<br>";
+      ?>
+    </main>
 
-  <footer class="flex h-[10%] flex-wrap items-center justify-center bg-zinc-500">
-    <div class="text-xl font-bold text-gray-700">© 2022 Copyright:</div>
-    <a class="ml-5 text-gray-900" href="https://github.com/pkubickii" target="_blank" rel="noreferrer">
-      pkubickii@github
-    </a>
-    <?php
-    require "hotreloader/HotReloader.php";
-    new HotReloader\HotReloader('//localhost/myphp/phrwatcher.php');
-    ?>
-  </footer>
+    <footer class="p-5 flex flex-wrap items-center justify-center bg-zinc-600">
+      <div class="text-xl font-bold text-gray-700">© 2022 Copyright:</div>
+      <a class="ml-5 text-gray-900" href="https://github.com/pkubickii" target="_blank" rel="noreferrer">
+        pkubicki
+      </a>
+      <?php
+      require "hotreloader/HotReloader.php";
+      new HotReloader\HotReloader('//localhost/myphp/phrwatcher.php');
+      ?>
+    </footer>
+  </div>
 </body>
 
 </html>
--- src/php/login.php
+++ src/php/login.php
@@ -1,2 +1,8 @@
 <?php
-echo "POSZLO Z BUTA";
+session_start();
+$username = $_POST["username"];
+$password = $_POST["password"];
+
+$_SESSION["username"] = $username;
+$_SESSION["password"] = password_hash($password, PASSWORD_DEFAULT);
+header("Location: /myphp/index.php");
